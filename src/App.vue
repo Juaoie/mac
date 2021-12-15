6715bdb734ba33b4fa911e02cb5cafdbf2b3356e
Juaoie <uaoie@qq.com>
2021-12-16 00:26:58 +0800

    使用get请求下载服务器资源示例

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div @contextmenu.prevent>
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    <a download="aaa.png" :href="url">点击下载</a>
+    <div @click="abc">点击下载fun</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import axios from "axios";
-async function init() {
-  const url =
-    "https://images.unsplash.com/photo-1639501158624-272ee4bb786d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60";
-  const file = await axios.get(url);
-  // window.open(file.data);
+// const url =
+//   "https://images.unsplash.com/photo-1639501158624-272ee4bb786d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60";
+const url = "http://zhidianyun.sdxxtop.com/classroom/video/20200727/20200727161946_qecm.mp4";
+async function abc() {
+  const file = await axios.get(url, {
+    responseType: "blob",
+  });
+  console.log("🚀 ~ file: App.vue ~ line 17 ~ init ~ file", file);
+  // const blob = new Blob(file.data);
   const a = document.createElement("a");
-  a.href = url;
-  a.download = "资源目录.png";
+  a.download = "abc.mp4";
+  a.href = URL.createObjectURL(file.data);
+  document.body.appendChild(a);
   a.click();
 }
-init();
 </script>
 <style>
 body {
